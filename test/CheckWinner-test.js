7956ecbb4f2e02f38f30a3363de6373de36651e6
Travis <tlgreg86@gmail.com>
2017-03-22 17:14:43 -0600

    Write more tests as well as style background, canvas, and pieces

--- test/CheckWinner-test.js
+++ test/CheckWinner-test.js
@@ -1,9 +1,11 @@
 let assert = require('chai').assert;
 let CheckForWinner  = require('../lib/CheckWinner')
 
+
 describe('CheckForWinner', function () {
 
   let checkForWinner = new CheckForWinner()
+
 
   it('should be a class', function () {
     assert.isFunction(CheckForWinner)
@@ -24,5 +26,5 @@ describe('CheckForWinner', function () {
   it('should have a function that returns false if player 2 does not get 4 in a row', function () {
     assert.equal(checkForWinner.checkLine(2, 2, 2, 1), false)
   });
-
+  
 })
--- test/pieces-test.js
+++ test/pieces-test.js
@@ -3,26 +3,38 @@ let Piece  = require('../lib/Piece')
 
 describe('Piece', function() {
 
+let piece = new Piece(100, 200)
+
   it('should be a class', function() {
+
     assert.isFunction(Piece)
   })
 
   it('should be an object', function() {
-    let piece = new Piece()
 
     assert.isObject(piece)
   })
 
   it('should have a startX position', function() {
-    let piece = new Piece(100)
 
     assert.equal(piece.startX, 100)
   })
 
   it('should have a startY position', function() {
-    let piece = new Piece(100, 200)
 
     assert.equal(piece.startY, 200)
+  })
+
+  it('should have pieceCount with default of 0', function() {
+
+    assert.equal(piece.pieceCount, 0)
+  })
+
+  it('should be a function that increments pieceCount by 1', function() {
+
+    assert.equal(piece.pieceCount, 0)
+    piece.stalemate();
+    assert.equal(piece.pieceCount, 1)
   })
 
 })
