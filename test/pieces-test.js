645f13ab7ec87df45f6f84869864fb7409a77403
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-03-23 14:07:26 -0600

    eval notes from alter-nate

--- test/pieces-test.js
+++ test/pieces-test.js
@@ -11,26 +11,36 @@ describe('Piece', function() {
   })
 
   it('should be an object', function() {
-
     assert.isObject(piece)
   })
 
   it('should have a startX position', function() {
-
     assert.equal(piece.startX, 100)
   })
 
   it('should have a startY position', function() {
-
     assert.equal(piece.startY, 200)
   })
 
   it('should have pieceCount with default of 0', function() {
-
     assert.equal(piece.pieceCount, 0)
   })
 
+  // not clear whats supposed to be happening
   it('should be a function that increments pieceCount by 1', function() {
+    var board = []
+    var columns = []
+
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
+    piece.set(player, column, startX);
 
     assert.equal(piece.pieceCount, 0)
     piece.stalemate();
