aca976e6cf5ffb0e12b83399753e027ee6765290
Adam Carpenter <adamcarpenter86@gmail.com>
2017-03-25 12:20:01 -0600

    moved all of column class into board, no need for column class anymore, created tests for checkForWinner, created one function to draw both characters, moved stalemate outside of piece constructor, removed popups from winner alert and now alerts and play again are directly loaded onto H1 of page

--- test/CheckWinner-test.js
+++ test/CheckWinner-test.js
@@ -1,10 +1,23 @@
 let assert = require('chai').assert;
 let CheckForWinner  = require('../lib/CheckWinner')
 
-
 describe('CheckForWinner', function () {
 
   let checkForWinner = new CheckForWinner()
+
+  let board = [
+    [1],
+    [1],
+    [1, 1, 1, 1],
+    [1, 2, 1, 2],
+    [2, 2, 2, 1],
+    [1, 2],
+    [2]
+  ]
+  
+  let c = null
+  let r = null
+
 
   it('should be a class', function () {
     assert.isFunction(CheckForWinner)
@@ -26,4 +39,19 @@ describe('CheckForWinner', function () {
     assert.equal(checkForWinner.checkLine(2, 2, 2, 1), false)
   });
 
+  it('should have a function that checks horizontally on the board for four of one player in a row', function () {
+    assert.equal(checkForWinner.checkHorizontal(c, r, board), 1)
+  })
+
+  it('should have a function that checks vertically on the board for four of one player in a row', function () {
+    assert.equal(checkForWinner.checkVerticle(c, r, board), 1)
+  })
+
+  it('should have a function that checks diagonally down and right on the board for four of one player in a row', function () {
+    assert.equal(checkForWinner.checkDownRight(c, r, board), 1)
+  })
+
+  it('should have a function that checks diagonally down and left on the board for four of one player in a row', function () {
+    assert.equal(checkForWinner.checkDownLeft(c, r, board), 2)
+  })
 })
--- test/column-test.js
+++ test/column-test.js
@@ -1,27 +1,27 @@
-let assert = require('chai').assert;
-let Column  = require('../lib/Column')
-
-describe('Column', function() {
-  let column = new Column(100, 200)
-
-  it('should be a class', function() {
-    assert.isFunction(Column)
-  })
-
-  it('should be an object', function() {
-    assert.isObject(column)
-  })
-
-  it('should have a startX position', function() {
-    assert.equal(column.startX, 100)
-  })
-
-  it('should have a startY position', function() {
-    assert.equal(column.startY, 200)
-  })
-
-  it('should have a column array', function() {
-    assert.deepEqual(column.colArr, [])
-  })
-
-})
+// let assert = require('chai').assert;
+// let Column  = require('../lib/Column')
+//
+// describe('Column', function() {
+//   let column = new Column(100, 200)
+//
+//   it('should be a class', function() {
+//     assert.isFunction(Column)
+//   })
+//
+//   it('should be an object', function() {
+//     assert.isObject(column)
+//   })
+//
+//   it('should have a startX position', function() {
+//     assert.equal(column.startX, 100)
+//   })
+//
+//   it('should have a startY position', function() {
+//     assert.equal(column.startY, 200)
+//   })
+//
+//   it('should have a column array', function() {
+//     assert.deepEqual(column.colArr, [])
+//   })
+//
+// })
